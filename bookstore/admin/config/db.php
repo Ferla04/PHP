7f3b4c779974767ab0a438db428ebe5ab6312b82
Ferla04 <fer.velas135@gmail.com>
2023-02-14 15:22:59 -0500

    error en credenciales

--- bookstore/admin/config/db.php
+++ bookstore/admin/config/db.php
@@ -2,29 +2,20 @@
 
 class ConnectionMysql {
 
-  private $server;
-  private $user;
-  private $password;
-  private $database;
   private $connection;
 
   public function __construct( $server, $user, $password, $database ){
 
-    $this->server = $server;
-    $this->user = $user;
-    $this->password = $password;
-    $this->database = $database;
-
     try{
-      $this->connection = new PDO( "mysql:host=$this->server;dbname=$this->database", $this->user, $this->password );
+      $this->connection = new PDO( "mysql:host=$server;dbname=$database", $user, $password );
       $this->connection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-
-    }catch( PDOException $error ){
-      return 'Database connection failed: '.$error;
-    }
       
+    }catch( \Throwable $error ){
+      print_r('Database connection failed: '.$error);
+    }
+    
   }
-
+  
   public function consult( $sql ){
     $query = $this->connection->prepare( $sql );// Mete la consulta en una variable
     $query->execute();
